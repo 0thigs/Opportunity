f6a973bdf6a53f12f1e9a2479f0480808b781edb
0thigs <machinimavlogs@gmail.com>
2023-10-05 17:14:02 -0300

    Update: Main Page UI - SSR & CSR

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import Logo from '../../../public/images/logo.png'
-import Item from '../components/item'
 import { createClient } from '@supabase/supabase-js';
+import MainPage from "../mainUI/page";
 
 export async function getItems() {
   const supabase = createClient(process.env.PUBLIC_SUPABASE_URL, process.env.SUPABASE_KEY);
@@ -21,31 +19,6 @@ export default async function Main() {
   const items = await getItems();
 
   return (
-    <div className="w-screen h-screen bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 flex flex-col justify-center items-center gap-20">
-      <header className="from-slate-900 via-slate-800 to-slate-900 border-b-gray-600 border-b w-full flex justify-start items-center p-10 absolute top-0 gap-1">
-        <h1 className="text-zinc-50 text-2xl font-bold tracking-tighter">
-          Opportunity
-        </h1>
-        <Image src={Logo} width={25} alt="Picture of the author" />
-      </header>
-      <main className="bg-white rounded-lg p-10 w-3/5 h-[600px] min-w-[350px] shadow-xl shadow-gray-900 flex flex-col">
-        <div className="flex flex-col justify-center gap-2 border-b pb-5">
-          <p className="text-lg font-semibold">
-            Nome do Programa
-          </p>
-          <input type="text" className="outline-none bg-gray-100 rounded-md py-2 px-4 text-md border" placeholder="Pesquisar" />
-        </div>
-        <section className="pt-3 h-full overflow-auto">
-          <p className="font-light text-gray-400 pb-1 text-lg">
-            Resultados:
-          </p>
-          <ul className="flex flex-col gap-2">
-            {items.map((item) => {
-              return <Item key={item.id} name={item.name} description={item.description} url={item.url}/>
-            })}
-          </ul>
-        </section>
-      </main>
-    </div>
+    <MainPage data={items}/>
   );
 }
