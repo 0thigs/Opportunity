7d94e02e96c1e9b569789a307453015ebfeac2e6
0thigs <machinimavlogs@gmail.com>
2023-11-01 19:12:23 -0300

    Update: Main page, Added Login link button

--- src/app/mainUI/page.jsx
+++ src/app/mainUI/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Logo from '../../../public/images/logo.png'
 import Item from '../../components/item'
 import { useState, useEffect } from "react";
-
+import Link from "next/link";
 
 export default function Main({data}) {
     const [input, setInput] = useState("")
@@ -22,11 +22,18 @@ export default function Main({data}) {
 
     return (
         <div className="w-screen h-screen bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 flex flex-col justify-center items-center gap-20">
-            <header className="from-slate-900 via-slate-800 to-slate-900 border-b-gray-600 border-b w-full flex justify-start items-center p-10 absolute top-0 gap-1">
-                <h1 className="text-zinc-50 text-2xl font-bold tracking-tighter">
-                    Opportunity
-                </h1>
-                <Image src={Logo} width={25} alt="Picture of the author" />
+            <header className="from-slate-900 via-slate-800 to-slate-900 border-b-gray-600 border-b w-full flex justify-between items-center p-10 absolute top-0 gap-1">
+                <div className="flex justify-center items-center">
+                    <h1 className="text-zinc-50 text-2xl font-bold tracking-tighter">
+                        Opportunity
+                    </h1>
+                    <Image src={Logo} width={25} alt="Picture of the author" />
+                </div>
+                <div>
+                    <Link className="text-zinc-50 font-bold text-xl hover:text-zinc-400 transition-all duration-150" href={"../loginUI"}>
+                        Login
+                    </Link>
+                </div>
             </header>
             <main className="bg-white rounded-lg p-10 w-3/5 h-[600px] min-w-[350px] shadow-xl shadow-gray-900 flex flex-col">
                 <div className="flex flex-col justify-center gap-2 border-b pb-5">
